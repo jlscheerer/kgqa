c27f9ddbc8961f5e44a92377cbb5fd0ee15bb24c
Jan Luca Scheerer <scheerer@cs.tum.edu>
2023-10-15 20:31:17 +0200

    Rewrite QueryParser

--- kgqa/QueryParser.py
+++ kgqa/QueryParser.py
@@ -1,187 +1,342 @@
-from typing import List, Dict, Tuple
-import re
-import traceback
+from enum import Enum, auto
+from typing import List, Union
+from .QueryLexer import (
+    Identifier,
+    LiteralToken,
+    NumericLiteral,
+    QueryLexer,
+    QueryLexerException,
+    SourceLocation,
+    StringLiteral,
+    Token,
+    TokenType,
+)
+from dataclasses import dataclass
 
 
-class Var:
-    def __init__(self, val):
-        self.v = val
-
-    def __eq__(self, other):
-        if isinstance(other, Var):
-            if other.v == self.v:
-                return True
-        return False
-
-    def __hash__(self):
-        return hash(self.v)
-
-    def get_str(self):
-        return "var_" + self.v.lower()
+@dataclass
+class Variable:
+    token: Identifier
+    name: str
 
     def __repr__(self):
-        return self.get_str()
+        return f"Variable({self.name})"
 
 
-def is_var(x):
-    return isinstance(x, Var)
+@dataclass
+class Constant:
+    token: LiteralToken
 
 
+@dataclass
+class StringConstant(Constant):
+    token: StringLiteral
+    value: str
+
+    def __repr__(self):
+        return f"Constant('{self.value}')"
+
+
+@dataclass
+class NumericConstant(Constant):
+    token: NumericLiteral
+    value: Union[int, float]
+
+    def __repr__(self):
+        return f"Constant({self.value})"
+
+
+class AggregationType(Enum):
+    COUNT = auto()
+    SUM = auto()
+    AVG = auto()
+    MAX = auto()
+    MIN = auto()
+
+
+@dataclass
+class Aggregation:
+    var: Variable
+    type_: AggregationType
+
+    def __repr__(self):
+        return f"{self.type_.name}({self.var})"
+
+
+HeadItem = Union[Variable, Aggregation]
+QueryHead = List[HeadItem]
+
+
+@dataclass
+class QueryAtom:
+    pass
+
+
+@dataclass
+class Predicate(QueryAtom):
+    predicate: Variable
+    arguments: List[Union[Variable, Constant]]
+
+    def __repr__(self):
+        return (
+            f"{self.predicate.name}({', '.join([str(arg) for arg in self.arguments])})"
+        )
+
+
+class FilterOp(Enum):
+    EQ = "="
+    NEQ = "!="
+    LE = "<"
+    LEQ = "<="
+    GE = ">"
+    GEQ = ">="
+
+
+@dataclass
+class Filter(QueryAtom):
+    lhs: Variable
+    op: FilterOp
+    rhs: Constant
+
+    def __repr__(self):
+        return f"{self.lhs} {self.op.value} {self.rhs}"
+
+
+@dataclass
 class ParsedQuery:
-    def __init__(self, head=None, clauses=None, filters=[], expressq=""):
-        # Head is a list of variables appearing in the head, e.g [Var.X, Var.Y]
-        self.head = head
-
-        # Clauses is a list of clauses. Each clause has the following structure:
-        # [subj, predicate_sentence, obj]
-        # where subj, obj are either a ""-string or a Var. abstract variable.
-        self.clauses = clauses
-
-        # Filters is a list of filters. Each filter has the followng structure.
-        # [subj, [<>=], numerical expression]
-        self.filters = filters
-
-        self.expressq = expressq
-
-    def is_valid(self):
-        if type(self.clauses) != list:
-            return False
-        for c in self.clauses:
-            if type(c) != str or len(c) != 3:
-                return False
-        return type(self.head) == list
+    head: QueryHead
+    body: List[QueryAtom]
 
 
-def get_all_anchor_strings(pq: ParsedQuery):
-    res = []
-    for s, _, o in pq.clauses:
-        if isinstance(s, str):
-            res.append(s)
-        if isinstance(o, str):
-            res.append(o)
-    return res
+class QueryParserException(Exception):
+    def __init__(self, token: Token, error: str):
+        self.token = token
+        self.error = error
 
 
 class QueryParser:
-    def __init__(self):
-        pass
+    _tokens: List[Token]
+    _cursor: int
+    _eof: int
 
-    def get_parsed_clasue_string(self, single_clause_raw):
-        """
-        Given a clause of the form "starred_in_a_movie(X,Y)", where X or
-        Y might be an anchor, returns a tuple:
-        (subject, predicate_in_english, object)
-        (X, "starred in a movie", Y)
-        """
-        # Strip last )
-        single_clause_raw = single_clause_raw[:-1]
+    def parse(self, query: str):
+        self._tokens = list(QueryLexer(query))
+        self._cursor = 0
+        self._eof = len(query)
+        head: QueryHead = []
+        if self._check_contains_colon():
+            head = self._parse_head()
+        body = self._parse_body()
+        return ParsedQuery(head=head, body=body)
 
-        # Get predicate, and s,o pair
-        pred_raw, so_pair = single_clause_raw.split("(")
+    def _parse_head(self) -> QueryHead:
+        token = self._pop_token()
+        # We should always at least encounter the `COLON` token.
+        assert token is not None
+        item: HeadItem
+        if token.token_type == TokenType.IDENTIFIER:
+            item = Variable(token=token, name=token.name)
+        elif token.token_type == TokenType.AGGREGATION_FUNCTION:
+            arguments = self._parse_arguments()
+            if len(arguments) == 0:
+                raise QueryParserException(token, "missing argument for aggregation")
+            elif len(arguments) != 1:
+                raise QueryLexerException(
+                    SourceLocation(
+                        begin=arguments[0].token.source_location.begin,
+                        end=arguments[-1].token.source_location.end,
+                    ),
+                    "too many arguments for aggregation",
+                )
 
-        # Replace underscores with spaces
-        predicate_sentence = " ".join(pred_raw.split("_"))
-
-        # Separate subject and object
-        if "," in so_pair:
-            # We have a binary predicate, e.g directed(M,D)
-            subj, obj = so_pair.split(",")
-            return [subj, predicate_sentence, obj]
+            var = arguments[0]
+            if not isinstance(var, Variable):
+                raise QueryParserException(
+                    var.token, f"unexpected {var} for aggregation"
+                )
+            item = Aggregation(var=var, type_=AggregationType[token.name])
         else:
-            # We have a unary predicate, e.g movie(M)
-            # Encode it as (subject, instance of, predicate)
-            # In this example, (M, "instance of", "movie")
-            return [so_pair, "instance of", f'"{predicate_sentence}"']
-
-    def get_parsed_filter_from_arithmetic(self, arithmetic):
-        x, op, const = arithmetic.split(" ")
-        return [Var(x), op, int(const)]
-
-    def parse(self, q: str):
-        input_query = q
-        # Preliminary parse
-        try:
-            heads_raw, body_raw = input_query.split(":")
-
-            # Match all patterns that look like:
-            # predicate(X,Y)
-            # separated by ;
-            clauses_raw = re.findall(r"[A-Za-z\_]+\([^;]+\)", body_raw)
-
-            # Thanks chatGPT. First one (March 24, 2023).
-            # I anticipate soon this codebase will be full of "thanks chatGPT!".
-            arithmetic_raw = re.findall(
-                r"(?:[A-Za-z]+)\s*[><=]+\s*(?:\d+(?:\.\d+)?[A-Za-z]*)", body_raw
+            raise QueryParserException(
+                token, f"unexpected {token.token_type} in query head"
             )
 
-            # Split head variables
-            assert "'" not in heads_raw
-            heads = heads_raw.split(",")
-            convert_head_tokens_to_Vars(heads)
-
-            # Parse each raw clause string
-            parsed_clauses, filters = [], []
-            for clause_raw in clauses_raw:
-                # Transform 'starred_in(X,Y)' --> [X, "starred in", Y]
-                # or.       'movie(M)'        --> [M, "movie"]
-                clause = self.get_parsed_clasue_string(clause_raw)
-                clause = [x.strip() for x in clause]
-
-                clause[0] = convert_to_Var_or_remove_double_quotes(clause[0])
-                if len(clause) == 3:
-                    # Binary
-                    clause[2] = convert_to_Var_or_remove_double_quotes(clause[2])
-                else:
-                    pass
-                parsed_clauses.append(clause)
-
-            for arithmetic in arithmetic_raw:
-                parsed_filter = self.get_parsed_filter_from_arithmetic(arithmetic)
-                filters.append(parsed_filter)
-
-            return ParsedQuery(
-                head=heads, clauses=parsed_clauses, filters=filters, expressq=q
+        token = self._pop_token()
+        # We should always at least encounter the `COLON` token.
+        assert token is not None
+        if token.token_type == TokenType.COMMA:
+            return [item] + self._parse_head()
+        elif token.token_type != TokenType.COLON:
+            raise QueryParserException(
+                token, f"unexpected {token.token_type} in query head"
             )
-        except Exception as e:
-            print(f"Exception occured in parsing {e}")
-            traceback.print_exc()
-            return ParsedQuery([], [], [], "")
+        return [item]
 
+    def _parse_body(self) -> List[QueryAtom]:
+        atoms: List[QueryAtom] = []
+        while True:
+            atom = self._parse_atom()
+            if atom is None:
+                raise QueryLexerException(
+                    SourceLocation(begin=self._eof, end=self._eof + 1),
+                    "missing query body",
+                )
+            atoms.append(atom)
+            if self._curr_token() is None:
+                break
+            token = self._pop_require_token()
+            if token.token_type != TokenType.SEMICOLON:
+                raise QueryParserException(
+                    token, f"unexpected {token.token_type} in query body"
+                )
+        return atoms
 
-def is_double_quoted_string(s):
-    """
-    Returns true if s is a string and has no outside double quotes.
-    is_double_quoted_string("\"some_string\"") = True
-    is_double_quoted_string("\"\"") = True
-    is_double_quoted_string("some_string") = False
-    is_double_quoted_string("\"some_string") = False
-    """
-    return isinstance(s, str) and len(s) >= 2 and s[0] == '"' and s[-1] == '"'
+    def _parse_atom(self):
+        ident = self._pop_token()
+        if ident is None:
+            return None
 
-
-def remove_double_quotes_if_present(s):
-    if is_double_quoted_string(s):
-        return s[1:-1]
-
-
-def is_token(s):
-    return isinstance(s, str) and len(s) == 1 and not is_double_quoted_string(s)
-
-
-def convert_to_Var_or_remove_double_quotes(s):
-    if is_token(s):
-        return Var(s)
-    elif is_double_quoted_string(s):
-        return remove_double_quotes_if_present(s)
-    else:
-        print(
-            f"Input {s} to a predicate is neither a token, nor a double quoted string."
+        if ident.token_type != TokenType.IDENTIFIER:
+            raise QueryParserException(
+                ident, f"unexpected {ident.token_type} in query body"
+            )
+        token = self._require_token()
+        if token.token_type == TokenType.OPEN_PAREN:
+            arguments = self._parse_arguments()
+            return Predicate(
+                predicate=Variable(token=ident, name=ident.name), arguments=arguments
+            )
+        elif token.token_type == TokenType.COMPARATOR:
+            op = self._pop_require_token()
+            if op.token_type != TokenType.COMPARATOR:
+                raise QueryParserException(
+                    op, f"expected {TokenType.COMPARATOR} got {op.token_type}"
+                )
+            rhs = self._pop_require_token()
+            if rhs.token_type not in [
+                TokenType.STRING_LITERAL,
+                TokenType.NUMERIC_LITERAL,
+            ]:
+                raise QueryParserException(
+                    rhs,
+                    f"expected {TokenType.STRING_LITERAL} | {TokenType.NUMERIC_LITERAL} got {rhs.token_type}",
+                )
+            return Filter(
+                lhs=Variable(token=ident, name=ident.name),
+                op=FilterOp(op.operator),
+                rhs=self._as_constant(rhs),
+            )
+        raise QueryParserException(
+            token, f"unexpected {token.token_type} in query body"
         )
-        assert False
 
+    def _as_constant(self, token: Token) -> Constant:
+        if token.token_type == TokenType.STRING_LITERAL:
+            return StringConstant(token=token, value=token.value)
+        elif token.token_type == TokenType.NUMERIC_LITERAL:
+            if "." in token.value:
+                return NumericConstant(token=token, value=float(token.value))
+            return NumericConstant(token=token, value=int(token.value))
+        # this represents an illegal invokation of _as_constant()
+        raise AssertionError()
 
-def convert_head_tokens_to_Vars(lst: List[str]):
-    n = len(lst)
-    for i in range(n):
-        if is_token(lst[i]):
-            lst[i] = Var(lst[i])
+    def _parse_arguments(self):
+        self._check_token(TokenType.OPEN_PAREN, should_raise=True)
+
+        arguments: List[Union[Variable, Constant]] = []
+        while True:
+            token = self._pop_token()
+            if token is None:
+                raise QueryLexerException(
+                    SourceLocation(begin=self._eof, end=self._eof + 1),
+                    "unterminated argument list",
+                )
+            if token.token_type == TokenType.CLOSE_PAREN:
+                return arguments
+
+            if token.token_type == TokenType.IDENTIFIER:
+                arguments.append(Variable(token=token, name=token.name))
+            elif token.token_type in [
+                TokenType.STRING_LITERAL,
+                TokenType.NUMERIC_LITERAL,
+            ]:
+                arguments.append(self._as_constant(token))
+            else:
+                raise QueryParserException(
+                    token, f"unexpected {token.token_type} in argument list"
+                )
+
+            token = self._pop_token()
+            if token is None:
+                raise QueryLexerException(
+                    SourceLocation(begin=self._eof, end=self._eof + 1),
+                    "unterminated argument list",
+                )
+            if token.token_type == TokenType.CLOSE_PAREN:
+                return arguments
+
+            if token.token_type != TokenType.COMMA:
+                raise QueryParserException(
+                    token, f"unexpected {token.token_type} in argument list"
+                )
+
+    def _check_contains_colon(self):
+        colon_tokens = [
+            *filter(lambda token: token.token_type == TokenType.COLON, self._tokens)
+        ]
+        if len(colon_tokens) > 1:
+            raise QueryParserException(colon_tokens[1], "extranous ':' in query")
+        return len(colon_tokens) == 1
+
+    def _advance_cursor(self, amount=1):
+        self._cursor += amount
+
+    def _token_at(self, cursor):
+        if cursor >= len(self._tokens):
+            return None
+        return self._tokens[cursor]
+
+    def _require_token(self):
+        token = self._curr_token()
+        if token is None:
+            raise QueryLexerException(
+                SourceLocation(begin=self._eof, end=self._eof + 1),
+                "missing require token",
+            )
+        return token
+
+    def _curr_token(self):
+        return self._token_at(self._cursor)
+
+    def _pop_require_token(self):
+        token = self._pop_token()
+        if token is None:
+            raise QueryLexerException(
+                SourceLocation(begin=self._eof, end=self._eof + 1),
+                "missing require token",
+            )
+        return token
+
+    def _pop_token(self):
+        token = self._curr_token()
+        self._advance_cursor()
+        return token
+
+    def _check_token(self, token_type: TokenType, should_raise: bool = False):
+        token = self._pop_token()
+        if token is None:
+            if should_raise:
+                raise QueryLexerException(
+                    SourceLocation(begin=self._eof, end=self._eof + 1),
+                    f"expected {token_type} but got EOF",
+                )
+            return False
+        if token.token_type != token_type:
+            if should_raise:
+                raise QueryParserException(
+                    token, f"expected {token_type} but got {token.token_type}"
+                )
+            return False
+        return True
+
+    def _peak_token(self):
+        return self._token_at(self._cursor + 1)
