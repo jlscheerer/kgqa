e3bef0a100f00aa50e818ecbf5e6b1545a84e51e
Jan Luca Scheerer <scheerer@cs.tum.edu>
2023-10-17 19:09:35 +0200

    Baseline Structure for SPARQLBackend

--- kgqa/PostProcessing.py
+++ kgqa/PostProcessing.py
@@ -3,17 +3,23 @@ import pandas as pd
 from kgqa.Database import Database
 from kgqa.QueryBackend import QueryString
 from kgqa.QueryGraph import ExecutableQueryGraph, QueryStatistics
+from kgqa.SPARQLBackend import SPARQLQuery
 from kgqa.SQLBackend import SQLQuery
 
 
 def run_and_rank(query: QueryString, wqg: ExecutableQueryGraph, stats: QueryStatistics):
-    assert isinstance(query, SQLQuery)
+    if isinstance(query, SQLQuery):
+        db = Database()
+        results, column_names = db.fetchall(query.value, return_column_names=True)
 
-    db = Database()
-    results, column_names = db.fetchall(query.value, return_column_names=True)
+        if column_names != [f"{column}" for column in stats.columns]:
+            raise AssertionError("received unexpected table format from database")
 
-    if column_names != [f"{column}" for column in stats.columns]:
-        raise AssertionError("received unexpected table format from database")
+        df = pd.DataFrame(results, columns=column_names)
+        return results, column_names
+    elif isinstance(query, SPARQLQuery):
+        print(query.value)
 
-    df = pd.DataFrame(results, columns=column_names)
-    return results, column_names
+        raise AssertionError
+    else:
+        raise AssertionError(f"cannot run_and_rank query '{query}'")
--- kgqa/SPARQLBackend.py
+++ kgqa/SPARQLBackend.py
@@ -16,7 +16,36 @@ class SPARQLBackend(QueryBackend):
     def to_query(
         self, stats: QueryStatistics, emit_labels: bool = False
     ) -> SPARQLQuery:
-        raise AssertionError
+        if emit_labels:
+            raise AssertionError("unsupported option 'emit_labels' for SPARQLBackend")
+
+        SELECT = self._construct_select()
+        WHERE = self._construct_where()
+        FILTER = self._construct_filter()
+        query = f"""SELECT {SELECT}
+                    WHERE
+                    {{
+                        {WHERE}
+                        FILTER({FILTER})
+                        SERVICE wikibase:label {{ bd:serviceParam wikibase:language "en". }}
+                    }}
+                 """
+
+        stats.set_column_info(self.columns)
+
+        return SPARQLQuery(value=query)
+
+    def _construct_select(self):
+        return "?X"
+
+    def _construct_where(self):
+        return "?X wdt:P57 wd:Q3772 ."
+
+    def _construct_filter(self):
+        if not self.requires_filters():
+            # TODO(jlscheerer) We need to check for empty here instead.
+            return "True"
+        return ""
 
 
 def wqg2sparql(
